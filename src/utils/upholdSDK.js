8b1761c695d52037165a833635f3b5e01c7013a2
Hugo Marques <hugomarquesdev@MacBook-Pro-de-Hugo.local>
2023-12-21 01:18:42 +0000

    Fix uphold-sdk

--- src/utils/upholdSDK.js
+++ src/utils/upholdSDK.js
@@ -1,7 +1,7 @@
 import SDK from "@uphold/uphold-sdk-javascript";
 
 const sdk = new SDK({
-    baseUrl: "http://api-sandbox.uphold.com",
+    baseUrl: "https://api-sandbox.uphold.com",
     clientId: "foo",
     clientSecret: "bar",
 });
